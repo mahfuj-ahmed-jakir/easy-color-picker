84f7cc3edeff2e30042970c5e918ce11648ceda1
Mahfuj Ahmed Jakir <mahfujahmedjakir@gmail.com>
2022-06-03 00:25:54 +0600

    v1.1.7 stable version release

--- README.md
+++ README.md
@@ -4,33 +4,37 @@
 * **Make Your Own** - You can add colors yourself and customize the design to your liking
 * It has been built for the convenience of development and design
 
-## Design Demo
+## Demo Design
 ![Demo](https://gcdnb.pbrd.co/images/aePPPObvnWlB.png)
-[**Live Demo**](http://mafhuj.me/)
+[**Live Demo**](http://mahfuj.me/)
 
 ## What is this?
 
 This is a react color picker. Easy Color Picker is a very nice package for developers and designers to grab colors from web pages or select from the color-picker board.
 
-## Installation & Usage
+## Installation
 
 ```
 npm i easy-color-picker --save
 ```
 
-## Include the Component
+## Usage
+
+* Use this `let [color, setColor] = useState("");` and `onClick={(code) => setColor(code)}` for single color get.
+* Use this `let [color, setColor] = useState([]);` and `onClick={(code) => setColor([code])}` for single color get with array.
+* Use this `let [color, setColor] = useState([]);` and `onClick={(code) => setColor([...color, code])}` for multiple color get with array.
 
 ```
 import React, { useState } from "react";
 import { ColorPicker } from "easy-color-picker";
 
 const Component = () => {
-  let [color, setColor] = useState([]);
+  let [color, setColor] = useState("");
 
   return (
-    <React.Fragment>
-      <ColorPicker selectOne={(color) => setColor(color)} />
-    </React.Fragment>
+    <>
+      <ColorPicker onClick={(code) => setColor(code)} />
+    </>
   );
 };
 ```
@@ -44,19 +48,24 @@ import { ColorPicker } from "easy-color-picker";
 const Component = () => {
   let [color, setColor] = useState([]);
 
-  let colors = ["Red", "Yellow", "Blue", "Black", "Orange", "Green", "White", "Purple", "Indigo", "Violet", "Gray", "Pink", "Gold", "Crimson", "Navy", "Teal", "YellowGreen", "Olive", "#000000", "#ffffff", "#f1f1f1", "#d9d9d9", "#202020", "#101010"];
+  let items = ["Red", "Yellow", "Blue", "Black", "Orange", "Green", "White"
+  "Purple", "Indigo", "Violet", "Gray", "Pink", "Gold", "Crimson", "Navy",
+  "Teal", "YellowGreen", "Olive", "#000000", "#ffffff", "#f1f1f1", "#d9d9d9",
+  "#202020", "#101010"];
 
   return (
-    <React.Fragment>
+    <>
       <ColorPicker
-      colors={colors}
-      background="#dddddd"
-      size="20px"
-      radius="2px"
-      width="658px"
-      selectOne={(color) => setColor(color)}
+        items={items}  // "items" for custom color add, it need must be [Array]
+        preview={color}  // "preview" for color review, here use color array for reveiw
+        background="#dddddd"  // "background" for customize background
+        size="30px"  // "size" for customize color size
+        radius="2px"  // "radius" for customize color radius
+        width="280px"  // "width" for customize background width
+        bgRadius="2px"  // "bgRadius" for customize background radius
+        onClick={(code) => setColor([...color, code])}  // "onClick" for get value
       />
-    </React.Fragment>
+    </>
   );
 };
 ```
--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,40 +1,74 @@
 import React from "react";
 
-const ColorPicker = ({ colors = ["Red", "Yellow", "Blue", "Black", "Orange", "Green", "White", "Purple", "Indigo", "Violet", "Gray", "Pink", "Gold", "Crimson", "Navy", "Teal", "YellowGreen", "Olive", "Magenta", "Maroon", "Orchid", "Chocolate", "DarkSlateGray", "Tomato"], background = "#f1f1f1", radius = "50%", size = "30px", width = "280px", selectOne }) => {
+const ColorPicker = ({ items = ["Red", "Yellow", "Blue", "Black", "Orange", "Green", "White", "Purple", "Indigo", "Violet", "Gray", "Pink", "Gold", "Crimson", "Navy", "Teal", "YellowGreen", "Olive", "Magenta", "Maroon", "Orchid", "Chocolate", "DarkSlateGray", "Tomato"], background = "#f1f1f1", radius = "50%", size = "30px", width = "280px", bgRadius = "2px", preview = [], onClick }) => {
   return (
-    <div
-      style={{
-        maxWidth: "1920px",
-        minWidth: "76px",
-        width: width,
-      }}
-    >
+    <>
       <div
         style={{
-          display: "flex",
-          flexWrap: "wrap",
-          borderRadius: "2px",
-          padding: "4px",
-          width: "auto",
-          background: background,
+          maxWidth: "1920px",
+          minWidth: "76px",
+          width: width,
+          marginBottom: "2px",
         }}
       >
-        {colors.map((color, index) => (
-          <div
-            onClick={() => selectOne(color)}
-            key={index}
-            style={{
-              margin: "2px",
-              background: color,
-              height: size,
-              width: size,
-              cursor: "pointer",
-              borderRadius: radius,
-            }}
-          ></div>
-        ))}
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            width: "auto",
+          }}
+        >
+          {preview.map((code, index) => (
+            <div
+              key={index}
+              style={{
+                margin: "2px",
+                background: code,
+                height: size,
+                width: size,
+                cursor: "pointer",
+                borderRadius: radius,
+                border: "2px solid rgba(0, 0, 0, 0.25)",
+              }}
+            ></div>
+          ))}
+        </div>
       </div>
-    </div>
+      {/* Show Color Code */}
+      <div
+        style={{
+          maxWidth: "1920px",
+          minWidth: "76px",
+          width: width,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            padding: "4px",
+            width: "auto",
+            borderRadius: bgRadius,
+            background: background,
+          }}
+        >
+          {items.map((code, index) => (
+            <div
+              key={index}
+              onClick={() => onClick(code)}
+              style={{
+                margin: "2px",
+                background: code,
+                height: size,
+                width: size,
+                cursor: "pointer",
+                borderRadius: radius,
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
